f5e5443f95ccd61903a7b25d55ccb9de3bca18a3
Ga6rielCorrea <gabriel2002correa@gmail.com>
2025-09-16 20:19:13 -0300

    feat: login page

---
+++ src/app/module/login/login.page.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectorRef,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  inject,
+} from '@angular/core';
+import { Router } from '@angular/router';
+import { ModalController } from '@ionic/angular/standalone';
+
+import { PushNotificationsService } from 'src/app/core/service/pushNotification.service';
+import { IonicComponentsModule } from 'src/app/shared/ionic-components.module';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  imports: [IonicComponentsModule, CommonModule],
+  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class LoginPage {
+  private _push = inject(PushNotificationsService);
+  private _modalCtrl = inject(ModalController);
+  private _router = inject(Router);
+  private _cdr = inject(ChangeDetectorRef);
+
+  constructor() {
+    this._push.requestPermissions();
+  }
+}
