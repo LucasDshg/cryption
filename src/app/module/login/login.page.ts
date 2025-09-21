1818e621fc392840718b76a3b08b6e48975b1f6c
Lucas Gomes <lucas.dshg@outlook.com>
2025-09-21 15:40:22 -0300

    fix: login

--- src/app/module/login/login.page.ts
+++ src/app/module/login/login.page.ts
@@ -13,12 +13,14 @@ import {
   Validators,
 } from '@angular/forms';
 import { NavController } from '@ionic/angular/standalone';
-import { finalize, switchMap, tap } from 'rxjs';
+import { finalize, Observable, switchMap, tap } from 'rxjs';
 import { IBot, ICorretora } from 'src/app/core/interfaces/user.interface';
 import { AuthService } from 'src/app/core/service/auth.service';
 import { UserStore } from 'src/app/core/store/user.store';
 
 import { IonicComponentsModule } from 'src/app/shared/ionic-components.module';
+import { IMe } from 'src/app/shared/robo/interface/me.interface';
+import { RoboService } from 'src/app/shared/robo/service/robo.service';
 
 @Component({
   selector: 'app-login',
@@ -37,13 +39,14 @@ import { IonicComponentsModule } from 'src/app/shared/ionic-components.module';
     FormsModule,
     ReactiveFormsModule,
   ],
-  providers: [NavController],
+  providers: [NavController, RoboService],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class LoginPage {
   private _authService = inject(AuthService);
   private _store = inject(UserStore);
   private _router = inject(NavController);
+  private _robo = inject(RoboService);
   private _tempLogin = signal<{ corretora: ICorretora; bot: IBot } | undefined>(
     undefined,
   );
@@ -68,21 +71,50 @@ export class LoginPage {
     this._authService
       .login(this.form.value.key!, this.form.value.code!)
       .pipe(
-        tap((res) => this._tempLogin.set(res)),
-        switchMap((res) => this._authService.corretora(res.corretora)),
-        tap((res) =>
-          this._store.update({
-            corretora: res.token,
-            id: this.form.value.code!,
-          }),
-        ),
-        switchMap(() => this._authService.robo(this._tempLogin()!.bot)),
-        tap((res) => this._store.update({ bot: res.token })),
+        tap((res) => {
+          this._tempLogin.set(res);
+        }),
+        switchMap(() => this._loginCorretora()),
+        switchMap(() => this._loginRobo()),
+        switchMap(() => this._getUserFromRobo()),
         finalize(() => this.loading.set(false)),
       )
       .subscribe(() => {
         this._tempLogin.set(undefined);
         this._router.navigateRoot('/home', { replaceUrl: true });
       });
+  }
+
+  private _loginRobo(): Observable<{ token: string }> {
+    return this._authService.robo(this._tempLogin()!.bot).pipe(
+      tap((res) => {
+        this._store.update({ bot: res.token });
+      }),
+    );
+  }
+
+  private _loginCorretora(): Observable<{ token: string }> {
+    return this._authService.corretora(this._tempLogin()!.corretora).pipe(
+      tap((res) => {
+        this._store.update({
+          corretora: res.token,
+          id: this.form.value.code!,
+        });
+      }),
+    );
+  }
+
+  private _getUserFromRobo(): Observable<IMe> {
+    return this._robo.me().pipe(
+      tap((res) => {
+        this._store.update({
+          robo: {
+            id: res.id,
+            loginId: res.loginId,
+            tenantId: res.tenantId,
+          },
+        });
+      }),
+    );
   }
 }
