0a810cf0c6edb8aaee4fe00022b89b7c03cca2b9
Lucas Gomes <lucas.dshg@outlook.com>
2025-09-21 15:51:59 -0300

    feat: bots page

--- src/app/shared/robo/interface/steup.interface.ts
+++ src/app/shared/robo/interface/steup.interface.ts
@@ -1,3 +1,61 @@
+export interface ISetupData {
+  id: string;
+  userId: string;
+  tenantId: string;
+  name: string;
+  description: string;
+  brokerProfileId: string;
+  brokerId: string;
+  brokerToken: string;
+  brokerUserId: string;
+  brokerMaxBalanceToUse: number;
+  brokerUserEmail: string;
+  brokerCopyTradeId: string;
+  active: boolean;
+  createdAt: string;
+  updatedAt: string;
+  connectionActive: boolean;
+  broker: {
+    id: string;
+    tenantId: string;
+    brokerTenantId: string;
+    name: string;
+    url: string;
+    imageUrl: string;
+    active: boolean;
+    maintenance: boolean;
+    comingSoon: boolean;
+    beta: boolean;
+    createdAt: string;
+    updatedAt: string;
+  };
+  brokerProfile: {
+    id: string;
+    profileTypeId: string;
+    tenantId: string;
+    brokerId: string;
+    brokerTraderId: string;
+    createdAt: string;
+    updatedAt: string;
+    profile: {
+      id: string;
+      tenantId: string;
+      name: string;
+      brokerId: string;
+      description: string;
+      risk: string;
+      averageMonthlyIncome: number;
+      maxAccountBalance: number;
+      imageUrl: string;
+      active: boolean;
+      createdAt: string;
+      updatedAt: string;
+    };
+  };
+  brokerUserBalance: number;
+  profit: number;
+}
+
 export interface ISetup {
   currentPage: number;
   perPage: number;
@@ -7,61 +65,5 @@ export interface ISetup {
   pages: number;
   total: number;
   count: number;
-  data: {
-    id: string;
-    userId: string;
-    tenantId: string;
-    name: string;
-    description: string;
-    brokerProfileId: string;
-    brokerId: string;
-    brokerToken: string;
-    brokerUserId: string;
-    brokerMaxBalanceToUse: number;
-    brokerUserEmail: string;
-    brokerCopyTradeId: string;
-    active: boolean;
-    createdAt: string;
-    updatedAt: string;
-    connectionActive: boolean;
-    broker: {
-      id: string;
-      tenantId: string;
-      brokerTenantId: string;
-      name: string;
-      url: string;
-      imageUrl: string;
-      active: boolean;
-      maintenance: boolean;
-      comingSoon: boolean;
-      beta: boolean;
-      createdAt: string;
-      updatedAt: string;
-    };
-    brokerProfile: {
-      id: string;
-      profileTypeId: string;
-      tenantId: string;
-      brokerId: string;
-      brokerTraderId: string;
-      createdAt: string;
-      updatedAt: string;
-      profile: {
-        id: string;
-        tenantId: string;
-        name: string;
-        brokerId: string;
-        description: string;
-        risk: string;
-        averageMonthlyIncome: number;
-        maxAccountBalance: number;
-        imageUrl: string;
-        active: boolean;
-        createdAt: string;
-        updatedAt: string;
-      };
-    };
-    brokerUserBalance: number;
-    profit: number;
-  };
+  data: ISetupData[];
 }
