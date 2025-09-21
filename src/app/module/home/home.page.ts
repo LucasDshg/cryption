fc591a7ac4a170e3d6808cd06609ef5459b28cbc
Lucas Gomes <lucas.dshg@outlook.com>
2025-09-21 15:02:25 -0300

    chore: foldder path

--- src/app/module/home/home.page.ts
+++ src/app/module/home/home.page.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { CorretoraService } from 'src/app/core/service/corretor.service';
 import { PushNotificationsService } from 'src/app/core/service/pushNotification.service';
 import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 import { MONTHS, MONTHS_DIC } from 'src/app/shared/constants/months.constants';
+import { CorretoraService } from 'src/app/shared/corretora/service/corretor.service';
 import { EMonths } from 'src/app/shared/enums/months.enum';
 import { IonicComponentsModule } from 'src/app/shared/ionic-components.module';
 
