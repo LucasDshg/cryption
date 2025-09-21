094e0f7aec5a3bd5d0f21e0b1d112ceff405328b
Lucas Gomes <lucas.dshg@outlook.com>
2025-09-21 17:30:47 -0300

    chore: setup android build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,23 +3,23 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'cryption',
-  webDir: 'www/browser',
+  webDir: 'www',
   backgroundColor: '#000000ff',
   android: {
-    adjustMarginsForEdgeToEdge: 'force'
+    adjustMarginsForEdgeToEdge: 'force',
   },
   plugins: {
     Keyboard: {
-      resizeOnFullScreen: false
+      resizeOnFullScreen: false,
     },
     SplashScreen: {
       launchShowDuration: 3000,
       launchAutoHide: true,
       androidScaleType: 'CENTER_CROP',
       splashImmersive: true,
-      backgroundColor: '#e77333'
-    }
-  }
+      backgroundColor: '#e77333',
+    },
+  },
 };
 
 export default config;
