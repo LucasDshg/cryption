0aa53c2965e9a797ee78a7dea7aa31faf69b4909
Lucas Gomes <lucas.dshg@outlook.com>
2025-09-21 22:19:19 -0300

    feat: using chart bar force component

--- src/app/module/home/home.page.ts
+++ src/app/module/home/home.page.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, computed, inject, signal } from '@angular/core';
 import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { PushNotificationsService } from 'src/app/core/service/pushNotification.service';
+import { ChartForceBarComponent } from 'src/app/shared/chart/force-bar/force-bar.component';
 import { CardLoadingComponent } from 'src/app/shared/components/card-loading/card-loading.component';
 import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 import { TradeItemComponent } from 'src/app/shared/components/trade-item/trade-item.component';
@@ -20,6 +21,7 @@ import { RoboService } from 'src/app/shared/robo/service/robo.service';
     HeaderComponent,
     TradeItemComponent,
     CardLoadingComponent,
+    ChartForceBarComponent,
   ],
   providers: [CorretoraService, RoboService],
 })
@@ -39,10 +41,25 @@ export class HomePage {
         end: MONTHS_DIC.get(params())!.end!,
       }),
   });
+
   readonly firstFiveTrades = computed(() => {
     const list = this.data.value()?.data;
     if (!list) return [];
     return list.slice(0, 5);
+  });
+
+  readonly totalWin = computed(() => {
+    const win = this.data.value()?.data.filter((it) => it.result !== 'LOST');
+    const price = win?.reduce((prev, curr) => curr.pnl + prev, 0);
+    const quant = win?.length;
+    return { price, quant };
+  });
+
+  readonly totalLoss = computed(() => {
+    const loss = this.data.value()?.data.filter((it) => it.result === 'LOST');
+    const price = loss?.reduce((prev, curr) => curr.pnl + prev, 0);
+    const quant = loss?.length;
+    return { price, quant };
   });
 
   readonly tradeInfo = toSignal(
@@ -57,10 +74,7 @@ export class HomePage {
   readonly wallet = computed(() => {
     const list = this.roboWallet();
     if (!list) return null;
-
-    return {
-      ...list[0],
-    };
+    return list[0];
   });
 
   constructor() {
