1d56dc286cd77f269eaa69337eb891f1a8d92d2d
Lucas Gomes <lucas.dshg@outlook.com>
2025-09-21 22:19:00 -0300

    feat: creat chart bar force component

---
+++ src/app/shared/chart/force-bar/force-bar.component.ts
@@ -0,0 +1,76 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, computed, model } from '@angular/core';
+import { IonicComponentsModule } from 'src/app/shared/ionic-components.module';
+
+@Component({
+  selector: 'app-chart-force-bar',
+  styles: [
+    `
+      .bar {
+        height: 30px;
+        color: white;
+        font-weight: 500;
+        padding: 4px 20px;
+        position: relative;
+        transition: width 300ms ease-in;
+      }
+      .win {
+        border-radius: 2rem 0 0 2rem;
+        background-color: var(--ion-color-success);
+        text-align: end;
+
+        &::after {
+          content: '%';
+          width: 30px;
+          height: 30px;
+          border-radius: 50%;
+          background-color: white;
+          position: absolute;
+          display: flex;
+          top: 0;
+          right: -15px;
+          z-index: 2;
+          color: var(--ion-color-dark);
+          align-items: center;
+          justify-content: center;
+          font-weight: 600;
+        }
+      }
+      .loss {
+        border-radius: 0 2rem 2rem 0;
+        background-color: var(--ion-color-danger);
+        text-align: start;
+      }
+    `,
+  ],
+  template: `
+    @defer (when total() && expenses()) {
+      <div class="ion-w-100 ion-d-flex ion-m-bottom-16">
+        <span class="ion-d-block bar win" [style.width.%]="win()">
+          {{ win().toFixed(2) }}
+        </span>
+        <span class="ion-d-block bar loss" [style.width.%]="loss()">
+          {{ loss().toFixed(2) }}
+        </span>
+      </div>
+      <ion-text [color]="total() + expenses() > 0 ? 'success' : 'danger'">
+        <p class="ion-fw-medium ion-text-nowrap">
+          {{ total() + expenses() | currency }}
+        </p>
+      </ion-text>
+    }
+  `,
+  imports: [IonicComponentsModule, CurrencyPipe],
+})
+export class ChartForceBarComponent {
+  readonly total = model.required<number>();
+  readonly expenses = model.required<number>();
+  readonly win = computed(() => {
+    const totalValue = Math.abs(this.expenses()) + this.total();
+    console.log(totalValue);
+    return 100 - (this.total() * 100) / totalValue;
+  });
+  readonly loss = computed(() => {
+    return (this.expenses() * 100) / this.total();
+  });
+}
